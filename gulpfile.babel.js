5c160e2b2fe4d37664bc8b8eeecc7638254d7765
Mark Chandler <mark.allen.chandler@gmail.com>
2016-08-23 21:25:44 -0400

    Add html gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,12 @@ import sass from 'gulp-sass'
 
 const dirs = {}
 
-gulp.task('default', ['sass'])
+gulp.task('default', ['html', 'sass'])
+
+gulp.task('html', () => {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('sass', () => {
   gulp.src('src/scss/**/*.scss')
